d03265e94c21d04afbc4c5ff3f8d63836162bde5
PavitraAneja <92890507+PavitraAneja@users.noreply.github.com>
2024-04-27 16:17:37 -0400

    Added login functionality

--- server.js
+++ server.js
@@ -23,7 +23,15 @@ app.use(express.static(__dirname + '../public'));
 
 /* GET home page, respond by rendering index.ejs */
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Add/Show Users' });
+  res.render('base', { title: 'Homepage' });
+});
+
+app.get('/create', function(req, res) {
+  res.render('index', { title: 'Add a new User' });
+});
+
+app.get('/main', function(req, res) {
+  res.render('main', { title: 'Give your symptoms' });
 });
 
 app.get('/success', function(req, res) {
@@ -33,6 +41,7 @@ app.get('/success', function(req, res) {
 // this code is executed when a user clicks the form submit button
 app.post('/mark', function(req, res) {
   var userid = req.body.userid;
+  var password  = req.body.password;
   var firstname = req.body.firstname;
   var lastname = req.body.lastname;
   var height = req.body.height;
@@ -40,17 +49,50 @@ app.post('/mark', function(req, res) {
   var sex = req.body.sex;
   var age = req.body.age;
   var race = req.body.race;
-  var sql = `INSERT INTO users (userid, firstname, lastname, height, weight, sex, age, race) VALUES ('${userid}','${firstname}','${lastname}','${height}','${weight}','${sex}','${age}','${race}')`;
+  var email = req.body.email;
+  var sqlcheck = 'SELECT COUNT(*) FROM users WHERE userid = ?';
+  var sql = `INSERT INTO users (userid, firstname, lastname, height, weight, sex, age, race, email, password) VALUES ('${userid}','${firstname}','${lastname}','${height}','${weight}','${sex}','${age}','${race}','${email}','${password}')`;
 
-
-
+  connection.query(sqlcheck, [userid], function(error, results) {
+    if (results[0]["COUNT(*)"] == 1) {
+      console.error('UserID already exists');
+      res.status(500).send('Sorry, User ID already exists. Try again!');
+      return;
+    } else {
+  
 console.log(sql);
   connection.query(sql, function(err, result) {
     if (err) {
       res.send(err)
       return;
     }
-    res.redirect('/success');
+    res.send('New user succesfully added!');
+  });}
+});
+});
+
+app.post('/login', function(req, res) {
+    var userid = req.body.userID;
+    var password = req.body.password;
+    var sqlcheck = 'SELECT COUNT(*) FROM users WHERE userid = ?';
+    var sql = 'SELECT password FROM users WHERE userid = ?';
+
+    connection.query(sqlcheck, [userid], function(error, results) {
+    if (results[0]["COUNT(*)"] == 0) {
+      console.error('UserID doesn\'t exist');
+      res.status(500).send('Sorry, User ID not found. Try again!');
+      return;
+    } else {
+
+  connection.query(sql, [userid], function(err, result) {
+    if (result[0]['password'] != [password]) {
+      console.error('Wrong password');
+      res.status(500).send('Sorry, wrong Password. Try again!');
+      return;
+    }
+    console.log('Received userID:', userid);
+    res.redirect('\create');
+  });}
   });
 });
 
